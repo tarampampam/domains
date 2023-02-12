ef518101b6d2c5af2dac4ab3bff4b60f12eeb9a2
sergeyverevkin <sergeyverevkin@gmail.com>
2023-02-12 18:11:13 +0700

    Create oop.is-an.app.js

---
+++ domains/oop.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Short name hosting for oop classes demo projects',
+  domain: 'is-an.app',
+  subdomain: 'oop',
+  owner: {
+    repo: 'http://vsa-jino.my.to:49272/pi20/ooya.ga',
+    email: 'sergeyverevkin@gmail.com',
+  },
+  record: {
+    CNAME: 'sergeyverevkin.github.io',
+  },
+  proxy: false,
+})
