e30e18373807b5955074865d452ac88e96be2ea7
yopremium21 <121346873+yopremium21@users.noreply.github.com>
2023-02-13 23:38:01 +0530

    Edited page to github pages (#115)
    * Create abhirockz.is-an.app.js
    
    * Update abhirockz.is-an.app.js
    
    * Update abhirockz.is-an.app.js

---
+++ domains/abhirockz.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'College project',
+  domain: 'is-an.app',
+  subdomain: 'abhirockz',
+  owner: {
+    repo: 'https://github.com/yopremium21/abhirockz',
+    email: 'yopremium21@gmail.com',
+  },
+  record: {
+    CNAME: 'yopremium21.github.io',
+  },
+  proxy: false,
+})
