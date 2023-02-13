e50173134186604706f8de9e0bb9107c0be281a7
yopremium21 <121346873+yopremium21@users.noreply.github.com>
2023-02-13 23:22:28 +0530

    Update abhirockz.is-an.app.js

--- domains/abhirockz.is-an.app.js
+++ domains/abhirockz.is-an.app.js
@@ -1,13 +1,13 @@
 addSubDomain({
-  description: 'For my college project', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'abhirockz', // desired subdomain name
+  description: 'College project',
+  domain: 'is-an.app',
+  subdomain: 'abhirockz',
   owner: {
-    repo: 'https://github.com/yopremium21',
+    repo: 'https://github.com/yopremium21/abhirockz',
     email: 'yopremium21@gmail.com',
   },
   record: {
-    TXT: ['google-site-verification=jfgLu1F8eWye93xVPU99SQt4MpCx6O_Sq8DAYz6hfFA']
+    CNAME: 'yopremium21.github.io',
   },
-    proxy: true,
+  proxy: false,
 })
