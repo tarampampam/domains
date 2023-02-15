045cabd7db0224119ddb01a74a8244ff8f8c4a30
max8330 <124245487+max8330@users.noreply.github.com>
2023-02-15 05:37:49 +0330

    Create compaxion.1tb.uk.js

---
+++ domains/compaxion.1tb.uk.js
@@ -0,0 +1,43 @@
+addSubDomain({
+
+  description: 'We are the CompaXion',
+
+  domain: '1bt.uk',
+
+  subdomain: 'compaxion',
+
+  owner: {
+
+    repo: 'https://github.com/max8330/free-domains/',
+
+    email: 'mosynikrooy@gmail.com',
+
+  },
+
+  record: {
+
+    A: [
+
+      '157.230.44.144',
+       ].
+    NS: [
+       'ns1.digitalocean.com'.
+       'ns2.digitalocean.com'.
+       'ns3.digitalocean.com'.
+        ].
+ 
+
+
+
+
+
+
+
+
+
+
+  },
+
+  proxy: false,
+
+})
