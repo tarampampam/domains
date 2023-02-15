b6adbdf81afa1f0e8a4e45223c74f98e4a4deca3
max8330 <124245487+max8330@users.noreply.github.com>
2023-02-15 10:02:53 +0330

    Create compaxion.1tb.uk.js (#116)
    * Create compaxion.1tb.uk.js
    
    * Create comp4xion.1tb.uk.js
    
    * Delete compaxion.1tb.uk.js
    
    * Rename comp4xion.1tb.uk.js to compaxion.1tb.uk.js
    
    * Rename compaxion.1tb.uk.js to compaxion.1bt.uk.js
    
    * Update compaxion.1bt.uk.js

---
+++ domains/compaxion.1bt.uk.js
@@ -0,0 +1,20 @@
+addSubDomain({
+  description: 'Compaxion, The power of Compression',
+  domain: '1bt.uk',
+  subdomain: 'compaxion',
+  owner: {
+    repo: 'https://github.com/smax8330/free-domains/',
+    email: 'mosynikrooy@gmail.com',
+  },
+  record: {
+    NS: [
+      'ns1.digitalocean.com.',
+      'ns2.digitalocean.com.',
+      'ns3.digitalocean.com.',
+    ],
+    A:  [
+      '157.230.44.144',
+    ],
+  },
+  proxy: false,
+})
