957350ecb3cd0956ec491586c241c1220ab93cdf
Haris <hariskumar.eth@gmail.com>
2023-02-17 10:30:50 +0530

    gravity updates

--- domains/gravity.is-an.app.js
+++ domains/gravity.is-an.app.js
@@ -3,10 +3,12 @@ addSubDomain({
     domain: 'is-an.app', 
     subdomain: 'gravity', 
     owner: {
+      repo: 'https://github.com/hariseth/gravity',
       email: 'hariskumar.eth@gmail.com',
     },
     record: {
       CNAME: 'cname.vercel-dns.com', 
+      TXT: ['vc-domain-verify=gravity.is-an.app', '445390ca003f9dec81e5'],
     },
    
   })
