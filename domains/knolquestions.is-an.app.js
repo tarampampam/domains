91206e513cc4f554d617036ce80d745f75a4c169
Kelvin-Lamptey <78008166+Kelvin-Lamptey@users.noreply.github.com>
2023-02-19 01:11:14 +0000

    Create knolquestions.is-an.app.js

---
+++ domains/knolquestions.is-an.app.js
@@ -0,0 +1,33 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: '...', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'knolquestions', // desired subdomain name
+  owner: {
+    repo: '<https://URL/to/the/repository/with/your/project/sources/if/it/is/public>',
+    email: 'kelvinlamptey77@gmail.com,
+  },
+  record: {
+    CNAME: 'kelvin-lamptey.github.io', // e.g.: <your-github-account>.github.io
+    TXT: ['list', 'of', 'required', 'txt', 'records'],
+    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
+    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
+    NS: ['list', 'of', 'nameservers'],
+  },
+  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
+                  // should provide valid a SSL certificate and protection CF will be disabled
+  // nested: [{ // in addition, you may define the required nested subdomains
+  //   subdomain: 'foo',
+  //   record: {
+  //     CNAME: '...',
+  //   },
+  //   proxy: false,
+  // }, {
+  //   subdomain: 'bar',
+  //   record: {
+  //     A: ['...'],
+  //   },
+  //   proxy: true,
+  // }]
+})
