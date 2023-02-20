f45bfe63b591f4027345ef66c1ee5b0c30ed9c3b
Kelvin-Lamptey <78008166+Kelvin-Lamptey@users.noreply.github.com>
2023-02-20 05:39:37 +0000

    Edit

--- domains/knolquestions.is-an.app.js
+++ domains/knolquestions.is-an.app.js
@@ -4,7 +4,7 @@ addSubDomain({
   subdomain: 'knolquestions',
   owner: {
     repo: 'https://github.com/Kelvin-Lamptey/KNOLquestions/',
-    email: 'kelvinlamptey77@gmail.com,
+    email: 'kelvinlamptey77@gmail.com',
   },
   record: {
     CNAME: 'kelvin-lamptey.github.io',
