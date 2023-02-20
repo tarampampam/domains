d0fd4d193935c2f6e23e88b94affa4eb944f1410
Kelvin-Lamptey <78008166+Kelvin-Lamptey@users.noreply.github.com>
2023-02-20 05:34:26 +0000

    Commit

--- domains/knolquestions.is-an.app.js
+++ domains/knolquestions.is-an.app.js
@@ -1,12 +1,12 @@
 addSubDomain({
-  description: '...', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'knolquestions', // desired subdomain name
+  description: '...',
+  domain: 'is-an.app',
+  subdomain: 'knolquestions',
   owner: {
     repo: 'https://github.com/Kelvin-Lamptey/KNOLquestions/',
     email: 'kelvinlamptey77@gmail.com,
   },
   record: {
-    CNAME: 'kelvin-lamptey.github.io', // e.g.: <your-github-account>.github.io
+    CNAME: 'kelvin-lamptey.github.io',
   },
 })
