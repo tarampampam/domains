048ba0114f74c8b9981980ebf0a46290ad12484f
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-21 10:27:57 +0300

    Create future.is-an.app.js

---
+++ domains/future.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'domain for my website',
+  domain: 'is-an.app',
+  subdomain: 'future',
+  owner: {
+    email: 'gslicocuk8080@hotmail.com',
+  },
+  record: {
+    CNAME: 'futuree.netlify.app',
+  },
+  proxy: false,
+})
