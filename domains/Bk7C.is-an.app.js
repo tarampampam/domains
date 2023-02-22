c6b0a0d5327b01570f3c044f934ebdabfd3ff464
Epikcoder <104178698+Epikcoder@users.noreply.github.com>
2023-02-22 09:57:58 +0700

    Create Bk7C.is-an.app.js (#123)

---
+++ domains/Bk7C.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: '599, a way to broadcast yourself',
+  domain: 'is-an.app',
+  subdomain: '599',
+  owner: {
+    repo: 'https://github.com/EpikCoder/free-domains/',
+    email: 'mrgamers11223@gmail.com',
+  },
+  record: {
+    CNAME: 'konseling-alhikmah-7c-kel-radith.000webhostapp.com'
+  },
+  proxy: false,
+})
