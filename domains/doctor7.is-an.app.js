3dc37796b46b1c936fba121c267c983700475304
Doctor Strange <108475621+DoctorStrange49@users.noreply.github.com>
2023-02-25 23:28:23 +0530

    Create doctor7.is-an.app.js (#131)

---
+++ domains/doctor7.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'College project',
+  domain: 'is-an.app',
+  subdomain: 'doctor7',
+  owner: {
+    repo: 'https://github.com/DoctorStrange49/free-domains',
+    email: 'stephen343strange@gmail.com',
+  },
+  record: {
+    CNAME: 'DoctorStrange49.github.io',
+  },
+  proxy: false,
+})
