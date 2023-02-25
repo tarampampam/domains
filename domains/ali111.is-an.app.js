962c85e0f4e334977000e51de915d53791398a91
ElonMusk49 <101656030+ElonMusk49@users.noreply.github.com>
2023-02-25 22:32:04 +0530

    Update ali111.is-an.app.js

--- domains/ali111.is-an.app.js
+++ domains/ali111.is-an.app.js
@@ -1,26 +1,27 @@
 addSubDomain({
-
   description: 'blogging',
-
   domain: 'is-an.app',
-
   subdomain: 'ali111',
-
   owner: {
-
     repo: 'https://github.com/ElonMusk49/free-domains',
-
     email: 'elonreevemusk201@gmail.com',
-
   },
-
   record: {
-
     CNAME: 'ElonMusk49.github.io',
-
   },
-
   proxy: false,
-
 })
+    
+
+
+
+  
+
+    
+
+  
+
+  
+
+
 
