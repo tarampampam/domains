eedd642173c097a4c6b218dbb7fb01b351435f93
Hüseyin Çınar <84516708+Huseyin-Cinar@users.noreply.github.com>
2023-02-26 16:50:48 +0300

    Create future.is-an.app.js

---
+++ domains/future.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'domain for my website',
+  domain: 'is-an.app',
+  subdomain: 'future',
+  owner: {
+    email: 'gslicocuk8080@hotmail.com',
+  },
+  record: {
+    CNAME: 'github.com/The-Future-Software-Company',
+  },
+  proxy: false,
+})
