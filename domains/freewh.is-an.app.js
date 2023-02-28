0f48dc582d212dc266f809d4f2fe911dadd8b299
OnionDev <roshan2008bothra@gmail.com>
2023-02-28 19:16:08 +0530

    Create freewh.is-an.app.js (#139)
    * Create freewh.is-an.app.js
    
    * Update freewh.is-an.app.js

---
+++ domains/freewh.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A free hosting website with a lot of features and good uptime',
+  domain: 'is-an.app',
+  subdomain: 'freewh',
+  owner: {
+    email: 'contact@onion.is-a.dev',
+  },
+  record: {
+    NS: ['ns1.freewh.in.eu.org', 'ns2.freewh.in.eu.org'],
+  },
+  proxy: false
+})
