d2a7c8079580dd61a9e44b514307fef036632c57
viral <viral.gadhiya@acquaintsofttech.com>
2023-03-03 17:05:32 +0530

    Register subdomain

---
+++ domains/viral-gadhiya.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'viral-gadhiya',
+  owner: {
+    repo: 'https://github.com/viral-gadhiya/viral-gadhiya.github.io,
+    email: 'viral.gadhiya@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'viral-gadhiya.github.io',
+  },
+  proxy: false,
+})
