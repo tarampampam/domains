89fe6280e95eb61cd641331e477dcd3e6db0ec8c
ajay-charotariya <ajay.charotariya@acquaintsofttech.com>
2023-03-03 18:03:04 +0530

    Add domain

---
+++ domains/ajay-charotariya.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'ajay-charotariya',
+  owner: {
+    repo: 'https://github.com/ajay-charotariya/ajay-charotariya.github.io',
+    email: 'ajay.charotariya@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'ajay-charotariya.github.io',
+  },
+  proxy: false,
+})
