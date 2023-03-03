72394631f21731cf8294af8f9c319bc08650ade1
mihir-patel <mihir.patel@acquaintsofttech.com>
2023-03-03 16:22:09 +0530

    Register my domain

---
+++ domains/mihir-patel-agc.is-an.app.js
@@ -0,0 +1,14 @@
+{
+  "$schema": "../schemas/domain.schema.json",
+  "description": "I want to publish my profile",
+  "domain": "is-an.app",
+  "subdomain": "mihir-patel-agc",
+  "owner": {
+    "repo": "https://github.com/mihir-patel-agc/mihir-patel-agc.github.io",
+    "email": "mihir.patel@acquaintsofttech.com"
+  },
+  "record": {
+    "CNAME": "mihir-patel-agc.github.io"
+  },
+  "proxy": false
+}
