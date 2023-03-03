cbe226cfc72e3fcb5fc59817d85473f395d1a5e9
ajit-thakor <ajit.thakor@acquaintsofttech.com>
2023-03-03 17:36:04 +0530

    add ajit domain

---
+++ domains/ajit-thakor.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'ajit-thakor',
+  owner: {
+    repo: 'https://github.com/ajit-thakor/ajit-thakor.github.io,
+    email: 'ajit.thakor@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'ajit-thakor.github.io',
+  },
+  proxy: false,
+})
