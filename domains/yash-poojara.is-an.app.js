e2157823ff27baca6ff3f8899a7deb810673684a
yash-poojara <yash.poojara@acquaintsoft.com>
2023-03-03 17:44:25 +0530

    my subdomain

---
+++ domains/yash-poojara.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'yash-poojara',
+  owner: {
+    repo: 'https://github.com/yash-poojara/yash-poojara.github.io,
+    email: 'yash.poojara@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'yash-poojara.github.io',
+  },
+  proxy: false,
+})
