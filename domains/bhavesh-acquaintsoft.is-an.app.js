1cc44e36d46425516703f0d0f9bb1e2c5725abe3
bhavesh <bhavesh.kagathara@acquaintsofttech.com>
2023-03-03 15:36:29 +0530

    Register my domain

---
+++ domains/bhavesh-acquaintsoft.is-an.app.js
@@ -0,0 +1,14 @@
+{
+  "$schema": "../schemas/domain.schema.json",
+  "description": "I want to publish my developer portfolio on this domain",
+  "domain": "is-an.app",
+  "subdomain": "bhavesh-acquaintsoft",
+  "owner": {
+    "repo": "https://github.com/bhavesh-acquaintsoft/bhavesh-acquaintsoft.github.io",
+    "email": "bhavesh.acquaintsoft@gmail.com"
+  },
+  "record": {
+    "CNAME": "bhavesh-acquaintsoft.github.io"
+  },
+  "proxy": false
+}
