2e8e79116cd4e879324f0c1808eb0db732abeb59
Ankit Dare <123159773+ankit-acquaintsoft@users.noreply.github.com>
2023-03-03 16:39:33 +0530

    change format

--- domains/ankit-acquaintsoft.is-an.app.js
+++ domains/ankit-acquaintsoft.is-an.app.js
@@ -1,14 +1,13 @@
-{
-  "$schema": "../schemas/domain.schema.json",
-  "description": "I want to publish my developer portfolio on this domain",
-  "domain": "is-an.app",
-  "subdomain": "ankit-acquaintsoft",
-  "owner": {
-    "repo": "https://github.com/ankit-acquaintsoft/ankit-acquaintsoft.github.io",
-    "email": "ankit@gmail.com"
+addSubDomain({
+  description: 'I want to publish my developer portfolio on this domain',
+  domain: 'is-an.app',
+  subdomain: 'ankit-acquaintsoft',
+  owner: {
+    repo: 'https://github.com/ankit-acquaintsoft/ankit-acquaintsoft.github.io',
+    email: 'ankit@gmail.com',
   },
-  "record": {
-    "CNAME": "ankit-acquaintsoft.github.io"
+  record: {
+    CNAME: 'ankit-acquaintsoft.github.io',
   },
-  "proxy": false
-}
+  proxy: false,
+})
