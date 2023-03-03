d391c316c03c648559adb80c4437f0b62ebcfcc4
ansari-saify <123363422+ansari-saify@users.noreply.github.com>
2023-03-03 16:42:03 +0530

    update format

--- domains/ansari-saify.is-an.app.js
+++ domains/ansari-saify.is-an.app.js
@@ -1,14 +1,13 @@
-{
-  "$schema": "../schemas/domain.schema.json",
-  "description": "I want to publish my profile",
-  "domain": "is-an.app",
-  "subdomain": "ansari-saify",
-  "owner": {
-    "repo": "https://github.com/ansari-saify/ansari-saify.github.io",
-    "email": "saif.ansari@acquaintsofttech.com"
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'ansari-saify',
+  owner: {
+    repo: 'https://github.com/ansari-saify/ansari-saify.github.io',
+    email: 'saif.ansari@acquaintsofttech.com',
   },
-  "record": {
-    "CNAME": "ansari-saify.github.io"
+  record: {
+    CNAME: 'ansari-saify.github.io',
   },
-  "proxy": false
-}
+  proxy: false,
+})
