e5a7b859a3a8d5117835ccbe0b011b88ada3fcd1
OnionDev <roshan2008bothra@gmail.com>
2023-03-03 16:29:30 +0530

    Change Domain to 1bt.uk

--- domains/freewh.1bt.uk.js
+++ domains/freewh.1bt.uk.js
@@ -1,6 +1,6 @@
 addSubDomain({
   description: 'A free hosting website with a lot of features and good uptime',
-  domain: 'is-an.app',
+  domain: '1bt.uk',
   subdomain: 'freewh',
   owner: {
     email: 'contact@onion.is-a.dev',
