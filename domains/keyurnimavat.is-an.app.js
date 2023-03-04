a91e4ed09bd36e7597561e89a661cb60d8318ac8
Keyur Nimavat <123536639+keyurnimavat@users.noreply.github.com>
2023-03-04 13:35:10 +0530

    add my domain (#169)

---
+++ domains/keyurnimavat.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'keyurnimavat',
+  owner: {
+    repo: 'https://github.com/keyurnimavat/keyurnimavat.github.io',
+    email: 'keyurnimavat@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'keyurnimavat.github.io',
+  },
+  proxy: false,
+})
