36910a5d42827bc97ee7db41da7eb3e6546a8923
grishma-chhayani <grishma.chhayani@acquaintsofttech.com>
2023-03-04 11:44:27 +0530

    Register domain

---
+++ domains/grishma-chhayani.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'grishma-chhayani',
+  owner: {
+    repo: 'https://github.com/grishma-chhayani/grishma-chhayani.github.io',
+    email: 'grishma.chhayani@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'grishma-chhayani.github.io',
+  },
+  proxy: false,
+})
