245c0d743df7687a4c71daafdc577f5f704b946d
rohit-sony <rohit.soni@acquaintsofttech.com>
2023-03-06 10:31:19 +0530

    my domain

---
+++ domains/rohit-sony.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'rohit-sony',
+  owner: {
+    repo: 'https://github.com/rohit-sony/rohit-sony.github.io',
+    email: 'rohit.sony@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'rohit-sony.github.io',
+  },
+  proxy: false,
+})
