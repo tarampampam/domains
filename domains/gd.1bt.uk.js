eac976cae054a37fe41d9068c4db24bb71c842e1
masckmaster2007 <63162857+masckmaster2007@users.noreply.github.com>
2023-03-09 21:19:28 +0100

    Create gd.1bt.uk.js (#197)
    * Create gd.1bt.uk.js
    
    * Syntax
    
    * rffdgfegfq
    
    * Final commit
    
    * back to desec

---
+++ domains/gd.1bt.uk.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Creating a reseller web hosting. Need a domain name. GD is shortened for Geometry Dash, because I know most of the people who will use this will be for making "Geometry Dash Fangame Servers"',
+  domain: '1bt.uk',
+  subdomain: 'gd',
+  owner: {
+    repo: 'https://github.com/masckmaster2007/free-domains',
+    email: 'contactus@dimisaio.x10.mx',
+  },
+  record: {
+    NS: ['ns1.desec.io', 'ns2.desec.org'],
+  },
+  proxy: true,
+})
