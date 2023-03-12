c50b028b154961940be487d6d81c06f67f0ac5dc
KavyanshKhaitan2 <73186427+KavyanshKhaitan2@users.noreply.github.com>
2023-03-12 10:19:09 +0530

    Update kjgames.is-an.app.js

--- domains/kjgames.is-an.app.js
+++ domains/kjgames.is-an.app.js
@@ -1,33 +1,14 @@
 // for more details watch the file `./domains.d.ts`
 
 addSubDomain({
-  description: 'JioGames by Kavyansh!', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'kjgames', // desired subdomain name
+  description: 'JioGames by Kavyansh!',
+  domain: 'is-an.app',
+  subdomain: 'kjgames',
   owner: {
     repo: '<https://URL/to/the/repository/with/your/project/sources/if/it/is/public>',
     email: '<your-public@email.address>',
   },
   record: {
-    CNAME: 'https://kavyanshkhaitan2.github.io/jiogames/', // e.g.: <your-github-account>.github.io
-    TXT: ['list', 'of', 'required', 'txt', 'records'],
-    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
-    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
-    NS: ['list', 'of', 'nameservers'],
+    CNAME: 'https://kavyanshkhaitan2.github.io/jiogames/',
   },
-  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
-                  // should provide valid a SSL certificate and protection CF will be disabled
-  // nested: [{ // in addition, you may define the required nested subdomains
-  //   subdomain: 'foo',
-  //   record: {
-  //     CNAME: '...',
-  //   },
-  //   proxy: false,
-  // }, {
-  //   subdomain: 'bar',
-  //   record: {
-  //     A: ['...'],
-  //   },
-  //   proxy: true,
-  // }]
 })
