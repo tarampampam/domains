82bd5a82de9e08d82c3c90a563fd6f7532ddcc98
Berlin Daemon ™ <127113967+Berlin-Daemon@users.noreply.github.com>
2023-03-15 07:07:38 -0600

    Create ltd.1bt.uk.js

---
+++ domains/ltd.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A fast and efficient golang proxy program',
+  domain: '1bt.uk',
+  subdomain: 'ltd',
+  owner: {
+    repo: '<https://github.com/Berlin-Daemon/ltd>',
+    email: '<berlin@id.ark.asso.nl.am>',
+  },
+  record: {
+    NS: ['georgia.ns.cloudflare.com', 'henrik.ns.cloudflare.com'],
+  },
+})
