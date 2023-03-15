e75579861e0f0f2d2c99c236639f185befbdc2be
zakariasekkat <sekkat@pre-history.com>
2023-03-15 13:06:00 +0100

    Create besport.is-an.app.js

---
+++ domains/besport.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to test some app',
+  domain: 'is-an.app',
+  subdomain: 'besport',
+  owner: {
+    repo: 'https://github.com/zakariasekkat',
+    email: 'bhavesh.acquaintsoft@gmail.com',
+  },
+  record: {
+    A: ['141.145.201.56'],
+  },
+  proxy: false,
+})
