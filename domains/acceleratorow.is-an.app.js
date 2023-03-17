ae922bac4a9a4cbf490d54f8f088f448eaba1541
Accelerator <tranhieu95@yahoo.com>
2023-03-17 18:54:29 +0700

    Update and rename acceleratorow.1bt.uk.js to acceleratorow.is-an.app.js

---
+++ domains/acceleratorow.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'The X-UI website on Oracle Free Tier VPS',
+  domain: 'is-an.app',
+  subdomain: 'acceleratorow',
+  owner: {
+    email: 'tranhieu95@yahoo.com',
+  },
+  record: {
+    A: ['140.83.81.12:54321'],
+  },
+})
