534b9ae99aa2296e1d2fa1aa5564c1e8fe1f68a7
SX Spy Agent <satriakrach@gmail.com>
2023-03-29 18:24:34 +0700

    Create codedit.is-an.app.js (#218)
    * Create codedit.is-an.app.js
    
    * Update codedit.is-an.app.js
    
    * Update codedit.is-an.app.js

---
+++ domains/codedit.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'CodedIt WebContainers',
+  domain: 'is-an.app', 
+  subdomain: 'codedit',
+  owner: {
+    repo: 'https://github.com/SX-9/codedit-web',
+    email: 'sx-91@outlook.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com',
+  },
+});
