c325581b73e05733744008f4d159e77bb60377db
Mirza Saikat Ahmmed <mirzasaikatahmmed@gmail.com>
2023-03-30 13:43:03 +0600

    Add my domain (#220)
    * Create saikat.is-an.app.js
    
    * Add my domain

---
+++ domains/saikat.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'saikat',
+  owner: {
+    repo: 'https://github.com/mirzasaikatahmmed/mirzasaikatahmmed.github.io',
+    email: 'info@mirzasaikatahmmed.info',
+  },
+  record: {
+    CNAME: 'mirzasaikatahmmed.github.io',
+  },
+  proxy: false,
+})
+ 
