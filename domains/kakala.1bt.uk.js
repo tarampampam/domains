266a7330450c83be6a5bcba5b3b9bb056b32a446
Leslie-Wong <manyu34@gmail.com>
2023-04-03 12:56:07 +0800

    Create kakala.1bt.uk.js (#224)

---
+++ domains/kakala.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A free hosting website with a lot of features and good uptime',
+  domain: '1bt.uk',
+  subdomain: 'kakala',
+  owner: {
+    email: 'leslie.wmy@gmail.com',
+  },
+  record: {
+    NS: ['NS1.DYNU.COM', 'NS7.DYNU.COM', 'NS9.DYNU.COM', 'NS11.DYNU.COM'],
+  },
+  proxy: false
+})
