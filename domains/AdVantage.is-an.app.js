d17360a8edda106c1bbade034f139eb2dc030299
Coderboi4 <103612036+Coderboi4@users.noreply.github.com>
2023-04-04 12:32:46 +1000

    Create AdVantage.is-an.app.js

---
+++ domains/AdVantage.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'AdVantage is an advertising company for discord bots. Basically a top.gg clone ',
+  domain: 'is-an.AdVantage',
+  subdomain: 'abhirockz',
+  owner: {
+    email: 'jayeshpoudel@outlook.com',
+  },
+  record: {
+    CNAME: '739116a3-2f7b-4c25-9f30-eb2546ca6bd5.id.repl.co',
+    TXT: 'replit-verify=739116a3-2f7b-4c25-9f30-eb2546ca6bd5',
+  },
+  proxy: false,
+})
