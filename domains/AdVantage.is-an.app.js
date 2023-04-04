ca29e4b453f3b084ceca03fa28ccad27f3a47999
Coderboi4 <103612036+Coderboi4@users.noreply.github.com>
2023-04-04 13:13:48 +1000

    Create AdVantage.is-an.app.js

---
+++ domains/AdVantage.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Project', // describe your project in this field
+  domain: '.is-an.app.js', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'AdVantage ', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/Coderboi4/free-domains',
+    email: 'Jayeshpoudel@outlook.com>',
+  },
+  record: {
+    CNAME: '739116a3-2f7b-4c25-9f30-eb2546ca6bd5.id.repl.co',
+    TXT: ['replit-verify=739116a3-2f7b-4c25-9f30-eb2546ca6bd5'],
+  
+  },
