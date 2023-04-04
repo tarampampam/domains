0126f1997e83f22d08d964c4bebd93d5352d390b
OntarioChess <129869921+OntarioChess@users.noreply.github.com>
2023-04-04 11:19:32 -0400

    Create OntarioChess.is-an.app.js (#227)

---
+++ domains/OntarioChess.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'We are an online chess organization based in Ontario, Canada. Established in April 2023, we host chess events for players located in Ontario, as well as those outside the province.',
+  domain: 'is-an.app',
+  subdomain: 'OntarioChess',
+  owner: {
+    email: 'ontariochess@coolsite.net',
+  },
+  record: {
+    CNAME: 'OntarioChess.github.io',
+  },
+  proxy: false,
+})
