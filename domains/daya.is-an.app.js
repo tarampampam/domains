55721483a3398a635fb4cdbac1a29feabd4071bb
SrEvelio <evelkiller2018@gmail.com>
2023-04-18 00:01:40 -0400

    Create daya.is-an.app.js (#229)
    Subdomain for Daya Discord bot web page, docs, and soon its cdn.

---
+++ domains/daya.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+    description: 'Website, cdn, docs, etc of my discord bot (https://top.gg/bot/808825161226780773)',
+    domain: 'is-an.app',
+    subdomain: 'daya',
+    owner: {
+        email: 'alphaevelio@gmail.com',
+    },
+    record: {
+        NS: ['ns1.afraid.org', 'ns2.afraid.org', 'ns3.afraid.org', 'ns4.afraid.org'],
+    },
+    proxy: false
+})
