d7b3e59d96e85cd661697be42aded30c2ad09ff0
savan-makawana <priyas.rana@truestaz.com>
2023-04-20 18:42:30 +0530

    Update Contant

---
+++ domains/service/savan-makawana.is-app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'I want to publish my profile',
+    domain: 'is-an.app',
+    subdomain: 'sawan-makawaba',
+    owner: {
+      repo: 'https://github.com/priyas-rana/priyas-rana.github.io',
+      email: 'priyas.rana@truestaz.com',
+    },
+    record: {
+      CNAME: 'savan-makawana.github.io',
+    },
+    proxy: false,
+  })
