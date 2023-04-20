b61556d7b826033e0ccb4359131d4298029fcf59
ujalakhasiya <ujala.khasiya@truestaz.com>
2023-04-20 18:57:02 +0530

    Added json file

---
+++ domains/ujalakhasiya.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'ujalakhasiya',
+  owner: {
+    repo: 'https://github.com/ujalakhasiya/ujalakhasiya.github.io',
+    email: 'ujala.khasiya@truestaz.com',
+  },
+  record: {
+    CNAME: 'ujalakhasiya.github.io',
+  },
+  proxy: false,
+})
