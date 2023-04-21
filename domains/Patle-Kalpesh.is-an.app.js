b8f4add012604761a0489a865939b1389962eade
Kalpesh Patel <129924554+Patle-Kalpesh@users.noreply.github.com>
2023-04-21 17:05:38 +0530

    Update Patle-Kalpesh.is-an.app.js

--- domains/Patle-Kalpesh.is-an.app.js
+++ domains/Patle-Kalpesh.is-an.app.js
@@ -1,13 +1,13 @@
 addSubDomain({
   description: 'I want to publish my profile',
   domain: 'is-an.app',
-  subdomain: 'Patle-Kalpesh',
+  subdomain: 'patle-kalpesh',
   owner: {
     repo: 'https://github.com/Patle-Kalpesh/Patle-Kalpesh.github.io',
     email: 'kalpesh.patel@truestaz.com',
   },
   record: {
-    CNAME: 'Patle-Kalpesh.github.io',
+    CNAME: 'patle-kalpesh.github.io',
   },
   proxy: false,
 })
