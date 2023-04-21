b353638d9acba1efcee375709e9f8bfeec24b884
ashish-g-gupta <ashish.gupta@truestaz.com>
2023-04-21 15:34:00 +0530

    Added json file

---
+++ domains/ashish-g-gupta.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'ashish-g-gupta',
+  owner: {
+    repo: 'https://github.com/ashish-g-gupta/ashish-g-gupta.github.io',
+    email: 'ashish.gupta@truestaz.com',
+  },
+  record: {
+    CNAME: 'ashish-g-gupta.github.io',
+  },
+  proxy: false,
+})
