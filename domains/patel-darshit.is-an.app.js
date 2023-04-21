a19a71a963b631b043de0d099538eb606c3ea444
Darshit Patel <129925775+Patel-Darshit@users.noreply.github.com>
2023-04-21 16:53:25 +0530

    Update and rename Patel-Darshit.is-an.app.js to patel-darshit.is-an.app.js

--- domains/patel-darshit.is-an.app.js
+++ domains/patel-darshit.is-an.app.js
@@ -1,13 +1,13 @@
 addSubDomain({
   description: 'I want to publish my profile',
   domain: 'is-an.app',
-  subdomain: 'Patel-Darshit',
+  subdomain: 'patel-darshit',
   owner: {
     repo: 'https://github.com/Patel-Darshit/Patel-Darshit.github.io',
     email: 'darshit.patel@truestaz.com',
   },
   record: {
-    CNAME: 'Patel-Darshit.github.io',
+    CNAME: 'patel-darshit.github.io',
   },
   proxy: false,
 })
