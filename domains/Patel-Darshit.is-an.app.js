38299f361124f9151fdc14d0cc3d849e3dd26045
Patel-Darshit <darshit.patel@truestaz.com>
2023-04-21 10:17:13 +0530

    Added json file

---
+++ domains/Patel-Darshit.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'Patel-Darshit',
+  owner: {
+    repo: 'https://github.com/Patel-Darshit/Patel-Darshit.github.io',
+    email: 'darshit.patel@truestaz.com',
+  },
+  record: {
+    CNAME: 'Patel-Darshit.github.io',
+  },
+  proxy: false,
+})
