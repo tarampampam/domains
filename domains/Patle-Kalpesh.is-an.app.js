a87f15fd2ac770ef366710c1c0bb62a5c5c8cb7b
Patle-Kalpesh <kalpesh.patel@truestaz.com>
2023-04-21 09:44:15 +0530

    Added json file

---
+++ domains/Patle-Kalpesh.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'Patle-Kalpesh',
+  owner: {
+    repo: 'https://github.com/Patle-Kalpesh/Patle-Kalpesh.github.io',
+    email: 'kalpesh.patel@truestaz.com',
+  },
+  record: {
+    CNAME: 'Patle-Kalpesh.github.io',
+  },
+  proxy: false,
+})
