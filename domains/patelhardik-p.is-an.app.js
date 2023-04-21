c6cbb9ec9c5ee83890a6e050d915cf0c195ae450
patelhardik-p <hardik.patel@truestaz.com>
2023-04-21 16:12:28 +0530

    Added json file

---
+++ domains/patelhardik-p.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'patelhardik-p',
+  owner: {
+    repo: 'https://github.com/patelhardik-p/patelhardik-p.github.io',
+    email: 'hardik.patel@truestaz.com',
+  },
+  record: {
+    CNAME: 'patelhardik-p.github.io',
+  },
+  proxy: false,
+})
