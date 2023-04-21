96b7e1f30c6e21b254b7105ebd474d25d87120cd
sanjay-makasana <jagruti.metaliya@truestaz.com>
2023-04-21 15:08:08 +0530

    Added json file

---
+++ domains/sanjay-makasana.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'sanjay-makasana',
+  owner: {
+    repo: 'https://github.com/sanjay-makasana/sanjay-makasana.github.io',
+    email: 'jagruti.metaliya@truestaz.com',
+  },
+  record: {
+    CNAME: 'sanjay-makasana.github.io',
+  },
+  proxy: false,
+})
