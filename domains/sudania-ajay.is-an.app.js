17d92fa93e9c9e3797e9a2f7d8de6986393e66e8
sudania-ajay <ajay.sudani@truestaz.com>
2023-04-21 15:17:07 +0530

    Added json file

---
+++ domains/sudania-ajay.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'sudania-ajay',
+  owner: {
+    repo: 'https://github.com/sudania-ajay/sudania-ajay.github.io',
+    email: 'ajay.sudani@truestaz.com',
+  },
+  record: {
+    CNAME: 'sudania-ajay.github.io',
+  },
+  proxy: false,
+})
