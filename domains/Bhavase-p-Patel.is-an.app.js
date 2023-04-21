d29da3d51cdfe17d16063eff31b1b452344959a9
Bhavesh-p-Patel <bhavesh.patel@truestaz.com>
2023-04-21 14:24:53 +0530

    Add .is-an.app.js File

---
+++ domains/Bhavase-p-Patel.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'I want to publish my profile',
+    domain: 'is-an.app',
+    subdomain: 'Bhavesh-p-Patel',
+    owner: {
+      repo: 'https://github.com/Bhavesh-p-Patel/Bhavesh-p-Patel.github.io',
+      email: 'bhavesh.patel@truestaz.com',
+    },
+    record: {
+      CNAME: 'Bhavesh-p-Patel.github.io',
+    },
+    proxy: false,
+  })
