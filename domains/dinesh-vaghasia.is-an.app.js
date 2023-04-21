33ff45daf93888e84bb0e6f00a925529bf6f6618
dinesh-vaghasia <dinesh.vaghasia@truestaz.com>
2023-04-21 14:43:11 +0530

    Add .is-an.app.js file

---
+++ domains/dinesh-vaghasia.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'dinesh-vaghasia',
+  owner: {
+    repo: 'https://github.com/dinesh-vaghasia/dinesh-vaghasia.github.io',
+    email: 'dinesh.vaghasia@truestaz.com',
+  },
+  record: {
+    CNAME: 'dinesh-vaghasia.github.io',
+  },
+  proxy: false,
+})
