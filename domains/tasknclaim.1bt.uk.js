fe86181ea557d64e25086d7cb650bc04d4903e38
Techiton <dynamicapidev@gmail.com>
2023-04-24 00:23:04 +1000

    Tasknclaim
    Created a file for the Tasknclaim subdomain.

---
+++ domains/tasknclaim.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Tasknclaim is a website where you can earn Gift Cards by Completing Surveys & More.',
+  domain: '1bt.uk',
+  subdomain: 'tasknclaim',
+  owner: {
+    email: "dynamicxau@gmail.com"
+  },
+  record: {
+    TXT: ["vc-domain-verify=tasknclaim.1bt.uk,d25a46a807fc4aeddbbd"]
+  },
+  proxy: false,
+})
