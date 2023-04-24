923b15eda21b70724b88f1f09f135ca0083c86b0
zaid-kadri <zaid.kadri@acquaintsofttech.com>
2023-04-24 12:38:40 +0530

    Added json file

---
+++ domains/zaid-kadri.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'zaid-kadri',
+  owner: {
+    repo: 'https://github.com/zaid-kadri/zaid-kadri.github.io',
+    email: 'zaid.kadri@acquaintsofttech.com',
+  },
+  record: {
+    CNAME: 'zaid-kadri.github.io',
+  },
+  proxy: false,
+})
