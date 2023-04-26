367f34a1f9e88ecffc996d5a80a2a9c5dc28ad51
Athif Rahmawan <123344700+Athif5025@users.noreply.github.com>
2023-04-26 16:57:34 +0700

    Create athif.is-an.app.js

---
+++ domains/athif.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'My personal portfolio', 
+  domain: 'is-an.app', 
+  subdomain: 'athif', 
+  owner: {
+    repo: 'https://github.com/Athif5025/MyProfile',
+    email: 'profriski10@gmail.com',
+  },
+  record: {
+    CNAME: 'athifrahmawan.github.io'
+  },
+  proxy: true,
+})
