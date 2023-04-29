2bdf59294d9b851e622c71f1743a7db2f1d109f4
Vien Catigan <vien@courvix.com>
2023-04-29 15:10:52 +0800

    Create maxlexpress.is-an.app.js

---
+++ domains/maxlexpress.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'We are a small business that delivers food like grub, we are located at the philippines ',
+  domain: 'is-an.app',
+  subdomain: 'maxlexpress',
+  owner: {
+    email: 'vien@courvix.com',
+  },
+  record: {
+    TXT: ['_github-pages-challenge-VienDeveloper.maxlexpress', 'a623dce85b2666dea6bc7b339529b6'],
+    CNAME: 'viendeveloper.github.io'
+  },
+  proxy: false,
+})
