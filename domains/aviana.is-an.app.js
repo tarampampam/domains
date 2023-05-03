21607ca72d701f9bc3eccee3159b447d0a9b5bde
Rygent <46462403+Rygent@users.noreply.github.com>
2023-05-03 16:48:51 +0700

    Register aviana.is-an.app (#300)

---
+++ domains/aviana.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my discord bot',
+  domain: 'is-an.app',
+  subdomain: 'aviana',
+  owner: {
+    email: 'contact@exylium.tk',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
