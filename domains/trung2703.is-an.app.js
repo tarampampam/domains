0cf384c17ae44bd6593872b1107fe42309340263
trung6689 <quan.lm203539@sis.hust.edu.vn>
2023-05-04 09:30:16 +0700

    Add trung2703 subdomain

---
+++ domains/trung2703.is-an.app.js
@@ -0,0 +1,14 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'I am a college student who is seeking a free domain name to create a website for educational purposes. As a student, I am always looking for ways to expand my knowledge and share my experiences with others. With a free domain name, I can create a platform to share my academic pursuits and connect with other students who share similar interests.',
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'trung2703', // desired subdomain name
+  owner: {
+    repo: '<https://github.com/trung6689/trung6689.github.io>',
+    email: '<trung2703@gmail.com>',
+  },
+  record: {
+    CNAME: '<trung6689.github.io>', // e.g.: <your-github-account>.github.io
+ },
+})
