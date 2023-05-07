90369464da634fad05504f3d110542a04c992a68
Quan <132177728+trung6689@users.noreply.github.com>
2023-05-08 01:00:26 +0700

    Update trung2703.is-an.app.js

--- domains/trung2703.is-an.app.js
+++ domains/trung2703.is-an.app.js
@@ -1,13 +1,13 @@
 addSubDomain({
-  description: 'I am a college student who is seeking a free domain name to create a website for educational purposes. As a student, I am always looking for ways to expand my knowledge and share my experiences with others. With a free domain name, I can create a platform to share my academic pursuits and connect with other students who share similar interests.',
-  domain: 'is-an.app', 
-  subdomain: 'trung2703', 
+  description: 'A website for my onedrive index',
+  domain: 'is-an.app',
+  subdomain: 'trung2703',
   owner: {
     repo: 'https://github.com/trung6689/onedrive-vercel-index',
     email: 'trung2703@gmail.com',
   },
   record: {
-    TXT: ['_vercel', 'vc-domain-verify=trung2703.is-an.app,3d86da4fc8d8694db89d'],
- },
+    CNAME: 'cname.vercel-dns.com'
+  },
   proxy: false
-})
+});
