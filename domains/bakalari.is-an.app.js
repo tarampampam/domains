21899719690aaba376df274f533c1ae10ed00ff3
Daniel Šebesta <dany.sebesta@seznam.cz>
2023-05-09 18:52:01 +0200

    Update bakalari.is-an.app.js

--- domains/bakalari.is-an.app.js
+++ domains/bakalari.is-an.app.js
@@ -1,9 +1,7 @@
-// for more details watch the file `./domains.d.ts`
-
 addSubDomain({
-  description: 'An intuitive and user-friendly alternative frontend for Bakaláři, the Czech school app used by teachers and students to monitor grades and timetables.', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'bakalari', // desired subdomain name
+  description: 'An intuitive and user-friendly alternative frontend for Bakaláři, the Czech school app used by teachers and students to monitor grades and timetables.',
+  domain: 'is-an.app',
+  subdomain: 'bakalari', 
   owner: {
     repo: 'https://github.com/danielsebesta/DastCMS',
     email: 'dast@smirkhat.org',
@@ -11,19 +9,5 @@ addSubDomain({
   record: {
     A: ['194.163.149.155'],
   },
-  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
-                  // should provide valid a SSL certificate and protection CF will be disabled
-  // nested: [{ // in addition, you may define the required nested subdomains
-  //   subdomain: 'foo',
-  //   record: {
-  //     CNAME: '...',
-  //   },
-  //   proxy: false,
-  // }, {
-  //   subdomain: 'bar',
-  //   record: {
-  //     A: ['...'],
-  //   },
-  //   proxy: true,
-  // }]
+ 
 })
