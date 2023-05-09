053c235a3caff42928d64f8da7e73442b3c793f8
Daniel Šebesta <dany.sebesta@seznam.cz>
2023-05-09 19:58:01 +0200

    Add bakalari.is-an.app (#313)

---
+++ domains/bakalari.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+  description: 'An intuitive and user-friendly alternative frontend for Bakaláři, the Czech school app used by teachers and students to monitor grades and timetables.',
+  domain: 'is-an.app',
+  subdomain: 'bakalari', 
+  owner: {
+    repo: 'https://github.com/danielsebesta/DastCMS',
+    email: 'dast@smirkhat.org',
+  },
+  record: {
+    A: ['194.163.149.155'],
+  },
+  proxy: false,
+ 
+})
