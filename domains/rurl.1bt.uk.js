ccb76ce792272e3689d375eb3aa8ea69f46505c2
Launchpad <admin@rdpstudio.top>
2023-05-12 04:46:12 +0800

    Create rurl.1bt.uk.js

---
+++ domains/rurl.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'FANGTANG Network Technology Short Links',
+  domain: '1bt.uk',
+  subdomain: 'rurl',
+  owner: {
+    // Content Complaints: tousu@rdpstudio.top
+    email: 'hello@rdpstudio.top',
+  },
+  record: {
+    CNAME: 'fbdd91ab-19c8-4d0a-9dff-0b2a9a68d29f.cfargotunnel.com'
+  },
+})
