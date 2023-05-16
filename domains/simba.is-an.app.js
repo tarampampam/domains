5ac7202ec9d4b3d5518cf264a2d9f8858d93fbba
Martin M <48408674+MartinTintin3@users.noreply.github.com>
2023-05-16 10:54:13 -0400

    Create simba.is-an.app.js

---
+++ domains/simba.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Some unblocked games',
+  domain: 'is-an.app',
+  subdomain: 'simba',
+  owner: {
+    repo: 'https://github.com/MartinTintin3/melvin-games',
+    email: 'maroymart@gmail.com',
+  },
+  record: {
+    CNAME: 'melvin4life.com',
+  },
+  proxy: false,
+})
