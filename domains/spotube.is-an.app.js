98408ffc7871ff7bb1ab2143d1700caf83438f06
Sir RaptaG <77157639+RaptaG@users.noreply.github.com>
2023-05-17 16:24:50 +0300

    Create spotube.is-an.app.js

---
+++ domains/spotube.is-an.app.js
@@ -0,0 +1,10 @@
+addSubDomain({
+  description: 'The domain for Spotube',
+  domain: 'is-an.app',
+  subdomain: 'spotube',
+  owner: {
+    repo: 'https://github.com/KRTirtho/spotube',
+  },
+  record: {
+    CNAME: 'spotube.netlify.app'
+})
