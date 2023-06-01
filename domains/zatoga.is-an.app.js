60a194e0639565e726e6fd9e262845f16a5c961b
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2023-06-01 08:11:43 -0400

    Create zatoga.is-an.app.js

---
+++ domains/zatoga.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Game website',
+  domain: 'is-an.app',
+  subdomain: 'zatoga',
+  owner: {
+    email: 'tamoghnak13@gmail.com',
+  },
+  record: {
+    CNAME: 'zatoga.pages.dev',
+  },
+  proxy: true,
+})
