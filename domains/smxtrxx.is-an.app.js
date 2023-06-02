4857b5b756a9a36fae7c92bb7897f767b7b61a2f
symmetricalbyte <73771649+symmetricalbyte@users.noreply.github.com>
2023-06-02 22:07:53 +0600

    Create smxtrxx.is-an.app.js

---
+++ domains/smxtrxx.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'personal server',
+  domain: 'is-an.app',
+  subdomain: 'smxtrxx',
+  owner: {
+    repo: 'none',
+    email: 'akibulamin@gmail.com',
+  },
+  record: {
+    A: '118.179.126.250',
+  },
+  proxy: false,
+})
