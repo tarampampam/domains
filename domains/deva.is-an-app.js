34a65a35e5712576b6a2ffaf79783caffa171dce
Deva Midhun <devamidhun.3c.kmbvm@gmail.com>
2023-06-17 11:55:50 +0530

    Create deva.is-an-app.js

---
+++ domains/deva.is-an-app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Personal website',
+  domain: 'is-an.app',
+  subdomain: 'deva',
+  owner: {
+    repo: 'https://github.com/turbomaster95/turbomaster95.github.io',
+    email: 'devamidhun.3c.kmbvm@gmail.com',
+  },
+  record: {
+    CNAME: 'turbomaster95.github.io'
+  },
+  proxy: false,
+})
