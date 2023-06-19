c401549e9a5cb6490f7de96d74d6e35c410206ec
AxewBoTX <lovedeepbrar54155@gmail.com>
2023-06-19 07:25:50 +0530

    commit

--- domains/calci.is-an.app.js
+++ domains/calci.is-an.app.js
@@ -1,31 +1,12 @@
 addSubDomain({
-  description: 'Its just a project website for a calculator app called, Calci', // describe your project in this field
-  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
-  subdomain: 'calci', // desired subdomain name
+  description: "Its just a project website for a calculator app called, Calci", // describe your project in this field
+  domain: "is-an.app", // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: "calci", // desired subdomain name
   owner: {
-    repo: 'https://github.com/AxewBoTX/calci',
-    email: 'lovedeepbrar54155@gmail.com',
+    repo: "https://github.com/AxewBoTX/calci",
+    email: "lovedeepbrar54155@gmail.com",
   },
   record: {
-    CNAME: 'axewbotx.github.io', // e.g.: <your-github-account>.github.io
-    TXT: ['list', 'of', 'required', 'txt', 'records'],
-    A: ['list', 'of', 'IPv4', 'addresses', 'like', 'a', '127.0.0.1'],
-    AAAA: ['list', 'of', 'IPv6', 'addresses', 'like', 'a', '::1'],
-    NS: ['list', 'of', 'nameservers'],
+    CNAME: "axewbotx.github.io", // e.g.: <your-github-account>.github.io
   },
-  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
-                  // should provide valid a SSL certificate and protection CF will be disabled
-  // nested: [{ // in addition, you may define the required nested subdomains
-  //   subdomain: 'foo',
-  //   record: {
-  //     CNAME: '...',
-  //   },
-  //   proxy: false,
-  // }, {
-  //   subdomain: 'bar',
-  //   record: {
-  //     A: ['...'],
-  //   },
-  //   proxy: true,
-  // }]
-})
+});
