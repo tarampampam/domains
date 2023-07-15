e2c99ff292a3a9357d6cd9904e37a9943dbca58b
NewStorm-0 <2456601397@qq.com>
2023-07-15 14:05:22 +0800

    Create newstorm.is-an.app.js
    Create newstorm.is-an.app.js

---
+++ domains/newstorm.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A personal website for me and my friends.',
+  domain: 'is-an.app',
+  subdomain: 'newstorm',
+  owner: {
+    email: 'NewStormCN@protonmail.com',
+  },
+  record: {
+    A: ['http:185.194.236.108'],
+  },
+  proxy: false
+})
