9f540096118c8fdc1318c50c13a2027e9e75b3a2
Kingq23 <80791284+Kingq23@users.noreply.github.com>
2023-07-15 21:57:53 -0400

    Create kingq23.is-an.app.js

---
+++ domains/kingq23.is-an.app.js
@@ -0,0 +1,17 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: '...', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'kingq23', // desired subdomain name
+  owner: {
+    
+    email: 'qadams2008@gmail.com',
+  },
+  record: {
+
+    A: [216.220.94.208],
+   
+  },
+
+})
