397e7b69a18139442fd557bbf7dfda4f85743f40
shapehouse <78955133+shapehouse@users.noreply.github.com>
2023-07-17 19:01:42 +0200

    Create shapehouse.is-an.app.js

---
+++ domains/shapehouse.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'shapehouse is a way to discover youself. a future based social media.', 
+  domain: 'is-an.app',
+  subdomain: 'shapehouse',
+  owner: {
+    repo: 'https://github.com/shapehouse/shapehouse',
+    email: 'hello@shapehouse.uu.me',
+  },
+  record: {
+    NS: ['ns1.byet.org', 'ns2.byet.org', 'ns3.byet.org', 'ns4.byet.org'],
+  },
+  proxy: false,
+})
