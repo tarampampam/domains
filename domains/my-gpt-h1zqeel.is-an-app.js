d45aee0e8eeacb87867b119d55b103eaf973af72
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-22 11:16:49 +0500

    My GPT Domain (#355)
    * domain
    
    * enable proxy

---
+++ domains/my-gpt-h1zqeel.is-an-app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+	description: 'A GPT Client that lets you consume GPT Models using your Personal OpenAI Key',
+	domain: 'is-an.app',
+	subdomain: 'my-gpt-h1zqeel',
+	owner: {
+		repo: 'https://github.com/h1zqeel/my-gpt',
+		email: 'hizqeeljaved2013@gmail.com',
+	},
+	record: {
+		A: ['18.159.114.201'],
+	},
+	proxy: true,
+})
