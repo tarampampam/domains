ede0bb0321f09b76ba190b5efc86f6ec5d96e7dd
Satwik G <102979033+satwikg3438@users.noreply.github.com>
2023-07-24 11:38:09 +0530

    Create giveaways.is-an.app.js
    My cool domain is very cool

---
+++ domains/giveaways.is-an.app.js
@@ -0,0 +1,13 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: 'Me and my friends have decided to have this website and host giveaways. it not illigal and pretty useful for us', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'giveaways', // desired subdomain name
+  owner: {
+    repo: '',
+    email: 'notsatwik374@gmail.com',
+  },
+  record: {
+    CNAME: '15818df6-965b-472c-b393-6a18d679678e.id.repl.co', // e.g.: <your-github-account>.github.io
+    },
