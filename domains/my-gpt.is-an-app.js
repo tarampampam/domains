7da7a9aa0633cd1756341f6f3daf758f1e8b3e13
Hizqeel Javed <hizqeeljaved2013@gmail.com>
2023-07-28 13:25:47 +0500

    update domain (#362)

--- domains/my-gpt.is-an-app.js
+++ domains/my-gpt.is-an-app.js
@@ -1,13 +1,13 @@
 addSubDomain({
 	description: 'A GPT Client that lets you consume GPT Models using your Personal OpenAI Key',
 	domain: 'is-an.app',
-	subdomain: 'my-gpt-h1zqeel',
+	subdomain: 'my-gpt',
 	owner: {
 		repo: 'https://github.com/h1zqeel/my-gpt',
 		email: 'hizqeeljaved2013@gmail.com',
 	},
 	record: {
-		A: ['18.159.114.201'],
+		CNAME: 'cname.vercel-dns.com'
 	},
 	proxy: true,
 })
