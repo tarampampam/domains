6f074640d89994988e833100f170fdedf3b43046
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2023-07-31 13:43:45 +0400

    Fix the file typo

---
+++ domains/my-gpt.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+	description: 'A GPT Client that lets you consume GPT Models using your Personal OpenAI Key',
+	domain: 'is-an.app',
+	subdomain: 'my-gpt',
+	owner: {
+		repo: 'https://github.com/h1zqeel/my-gpt',
+		email: 'hizqeeljaved2013@gmail.com',
+	},
+	record: {
+		CNAME: 'cname.vercel-dns.com'
+	},
+	proxy: true,
+})
