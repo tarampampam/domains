b05b4f364e8c91d4e0669c73260509a7f2b45904
Sayantan Paul <69689739+legendsayantan@users.noreply.github.com>
2023-08-08 10:47:21 -0700

    Create rewards.is-an.app.js (#365)

---
+++ domains/rewards.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'A website to collect rewards points easily.',
+  domain: 'is-an.app',
+  subdomain: 'rewards',
+  owner: {
+    repo: 'https://github.com/legendsayantan/rewards-website',
+    email: 'legendsayantan@gmail.com',
+  },
+  record: {
+    CNAME: 'legendsayantan.github.io',
+  },
+  proxy: false,
+})
