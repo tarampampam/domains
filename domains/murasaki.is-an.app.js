6e26a47eda9747b508cb462b02633a3ec71c2e18
Oreki <77574238+Oreki-Dev@users.noreply.github.com>
2023-08-14 20:42:34 +0530

    Create murasaki.is-an.app.js (#366)

---
+++ domains/murasaki.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Anime/Manga Blogging Platform',
+  domain: 'is-an.app',
+  subdomain: 'murasaki',
+  owner: {
+    repo: '',
+    email: 'orekidev@gmail.com',
+  },
+  record: {
+    NS: ['ns41.cloudns.net', 'ns42.cloudns.net', 'ns43.cloudns.net', 'ns44.cloudns.net'],
+  },
+})
