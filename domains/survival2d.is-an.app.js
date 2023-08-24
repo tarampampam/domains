b6e3318961116baad62873b0dfd3e9dbe1361dcd
TienNM5 <tiennm5@vng.com.vn>
2023-08-24 12:29:33 +0700

    [survival2d] (Add) register survival2d subdomain for my game project

---
+++ domains/survival2d.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Survival2D is a 2D battle royale game.',
+  domain: 'is-an.app',
+  subdomain: 'survival2d',
+  owner: {
+    repo: 'https://github.com/tiennm99/free-domains',
+    email: 'minhtienit99@gmail.com',
+  },
+  record: {
+    CNAME: 'survival2d.pages.dev',
+  },
+  proxy: false,
+})
