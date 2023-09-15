2777cf19e0c17ebe919057bc996b8b1e3964c044
Themadpunter <106832853+familycomicsstudios@users.noreply.github.com>
2023-09-15 12:41:09 -0500

    Create atcoin.is-an.app.js

---
+++ domains/atcoin.is-an.app.js
@@ -0,0 +1,30 @@
+// for more details watch the file `./domains.d.ts`
+
+addSubDomain({
+  description: '...', // describe your project in this field
+  domain: 'is-an.app', // aka "root-domain". select between '1bt.uk' and 'is-an.app'
+  subdomain: 'atcoin', // desired subdomain name
+  owner: {
+    repo: 'https://github.com/familycomicsstudios/atcoin',
+    email: 'themadpunter10@gmail.com',
+  },
+  record: {
+    CNAME: '89c4cc2d-c519-4490-99d0-2ebc45f3ce20.id.repl.co', // e.g.: <your-github-account>.github.io
+    TXT: ['replit-verify=89c4cc2d-c519-4490-99d0-2ebc45f3ce20']
+  },
+  //proxy: false, // disable Cloudflare proxy (with is enabled by default). In this case, your origin server
+                  // should provide valid a SSL certificate and protection CF will be disabled
+  // nested: [{ // in addition, you may define the required nested subdomains
+  //   subdomain: 'foo',
+  //   record: {
+  //     CNAME: '...',
+  //   },
+  //   proxy: false,
+  // }, {
+  //   subdomain: 'bar',
+  //   record: {
+  //     A: ['...'],
+  //   },
+  //   proxy: true,
+  // }]
+})
