f12467cec035ce16fbac4f6b32e7441192b81981
Priyansh Prajapat <priyansh029ztn@gmail.com>
2023-10-09 13:26:22 +0530

    Create devprofiles.is-an.app.js (#372)

---
+++ domains/devprofiles.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'list of devevlopers profiles',
+  domain: 'is-an.app',
+  subdomain: 'devprofiles',
+  owner: {
+    email: 'oyepriyansh@hotmail.com',
+    repo: 'https://github.com/oyepriyansh/DevProfiles',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
