b2b5535c37044e5b49014633ba7c4f8392302201
SrEvelio <evelkiller2018@gmail.com>
2024-01-03 15:43:40 -0400

    Update daya.is-an.app.js (#380)

--- domains/daya.is-an.app.js
+++ domains/daya.is-an.app.js
@@ -1,21 +1,21 @@
 addSubDomain({
-    description: 'Website, cdn, docs, etc of my discord bot (https://top.gg/bot/808825161226780773)',
+    description: 'Website, docs, of my discord bot (https://top.gg/bot/808825161226780773)',
     domain: 'is-an.app',
     subdomain: 'daya',
     owner: {
         email: 'alphaevelio@gmail.com',
     },
     record: {
-        CNAME: "daya.is-an.app.gigalixirdns.com",
+         A: ["216.24.57.1"],
     },
-    proxy: false,
+    proxy: true,
     nested: [
         {
             subdomain: "docs",
             record: {
                 CNAME: "74f89ed3f2-hosting.gitbook.io"
             },
-            proxy: false
+            proxy: true
         }
     ]
 })
