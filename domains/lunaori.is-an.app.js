4a023457826d585de5db2803c8076b37bf745c78
Oreki <77574238+OreOreki@users.noreply.github.com>
2024-03-11 11:11:15 +0530

    Added lunaori

---
+++ domains/lunaori.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: "Lunaori is an app by a weeb developer for weeb developers",
+  domain: "is-an.app",
+  subdomain: "lunaori",
+  owner: {
+    repo: "",
+    email: "orekidev@gmail.com",
+  },
+  record: {
+    CNAME: "cname.vercel-dns.com",
+  },
+  proxy: false,
+});
