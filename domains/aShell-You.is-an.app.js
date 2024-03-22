15c30b19bc84b32a49339318fee9fbab0ce4fef8
Krishna <125943937+Krishna-G-OP@users.noreply.github.com>
2024-03-22 07:45:52 +0530

    Create aShell-You.is-an.app.js

---
+++ domains/aShell-You.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+  description: 'A beautiful local ADB shell specifically designed for Android devices powered by Shizuku.
+',
+  domain: 'is-an.app',
+  subdomain: 'aShell-You',
+  owner: {
+    repo: 'https://github.com/Krishna-G-OP/aShellYou-Website',
+    email: 's.banti1357@gmail.com',
+  },
+  record: {
+    CNAME: 'https://krishna-g-op.github.io/aShellYou-Website/',
+  },
+  proxy: false,
+})
