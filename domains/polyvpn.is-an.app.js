65e8cfa5717f63b7492fd3ff65cafc55b2f9e0e2
PolyAnthi <79588299+PolyAnthi@users.noreply.github.com>
2024-04-20 09:27:59 +0100

    Create polyvpn.is-an.app.js

---
+++ domains/polyvpn.is-an.app.js
@@ -0,0 +1,14 @@
+addSubDomain({
+  description: 'VPN Project',
+  domain: 'is-an.app',
+  subdomain: 'polyvpn',
+  owner: {
+    discord: 'polyanthi',
+    email: 'polyanthi_pumicing@aleeas.com',
+  },
+  record: {
+    NS: 'ns1.desec.io',
+    NS: 'ns2.desec.org'
+  },
+  proxy: false,
+})
