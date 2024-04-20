e94123c9f13af98a31f2ff7e8083049eb4b9bd8e
PolyAnthi <79588299+PolyAnthi@users.noreply.github.com>
2024-04-20 21:27:49 +0100

    Update polyvpn.is-an.app.js

--- domains/polyvpn.is-an.app.js
+++ domains/polyvpn.is-an.app.js
@@ -3,7 +3,7 @@ addSubDomain({
   domain: 'is-an.app',
   subdomain: 'polyvpn',
   owner: {
-    email: 'polyanthi_pumicing@aleeas.com', // if needed discord contact, https://discord.gg/ux4Dkerw2h
+    email: 'polyanthi_pumicing@aleeas.com', // if needed discord contact, https://discord.gg/ux4Dkerw2h or 'polyanthi' on discord
   },
   record: {
     NS: ['ns1.desec.io', 'ns2.desec.org'],
