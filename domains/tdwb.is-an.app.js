09099fe1b9bef3be14f4b348ccedcdfbedffdb08
Themadpunter <106832853+familycomicsstudios@users.noreply.github.com>
2024-07-25 13:09:29 -0500

    Create tdwb.is-an.app.js

---
+++ domains/tdwb.is-an.app.js
@@ -0,0 +1,14 @@
+
+
+addSubDomain({
+  description: 'A top-down .io hero shooter game.',
+  domain: 'is-an.app',
+  subdomain: 'tdwb',
+  owner: {
+    repo: 'https://github.com/Themadpunter-Studios/tdwb',
+    email: 'themadpunter@proton.me',
+  },
+  record: {
+    CNAME: 'themadpunter-studios.github.io'
+  }
+})
